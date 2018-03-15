5d45c1fb549090fd90c2920a78ec70dd5e4418d3
ColormanGit <ColormanSoft@gmail.com>
2018-03-15 15:49:01 +0000

    2nd commit

---
+++ SPV_Loaders/SPV_Minerva_Loader/SPV_Minerva_Loader/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPV_Minerva_Loader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // your code here 
+                string CSVFilePathName = @"C:\SoftwareProjects\SPV_Loaders\SPV_Minerva_Loader\SPV_Minerva_Loader\bin\Debug";
+                string[] Lines = File.ReadAllLines(CSVFilePathName);
+                string[] Fields;
+                Fields = Lines[0].Split(new char[] { ',' });
+                int Cols = Fields.GetLength(0);
+                DataTable dt = new DataTable();
+                //1st row must be column names; force lower case to ensure matching later on.
+                for (int i = 0; i < Cols; i++)
+                    dt.Columns.Add(Fields[i].ToLower(), typeof(string));
+                DataRow Row;
+                for (int i = 1; i < Lines.GetLength(0); i++)
+                {
+                    Fields = Lines[i].Split(new char[] { ',' });
+                    Row = dt.NewRow();
+                    for (int f = 0; f < Cols; f++)
+                        Row[f] = Fields[f];
+                    dt.Rows.Add(Row);
+                }
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error is " + ex.ToString());
+                throw;
+            }
+        }
+    }
+}
